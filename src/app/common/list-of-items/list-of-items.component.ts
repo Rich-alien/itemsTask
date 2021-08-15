0217729c15f144d0d95d69cfb4b053fba1962923
richa <Richalien@yandex.ru>
2021-08-15 16:39:23 +0300

    delete console log

--- src/app/common/list-of-items/list-of-items.component.ts
+++ src/app/common/list-of-items/list-of-items.component.ts
@@ -18,7 +18,6 @@ export class ListOfItemsComponent implements OnInit {
   ngOnInit(): void {
     this.itemsService.postItemsData().subscribe(
       (body: any) => {
-        console.log(body);
         this.dataItems = (body.data.metering_devices.data as Array<Object>);
       },
       error => {
